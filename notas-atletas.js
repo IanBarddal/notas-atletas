caf203f862c77facc700f20eeb69abfa1f968fa7
Ian Barddal <136401651+IanBarddal@users.noreply.github.com>
2024-06-07 19:03:11 -0300

    Add files via upload

---
+++ notas-atletas.js
@@ -0,0 +1,46 @@
+function calcularMedia (notas)
+{
+    notas.sort((a, b) => a - b);
+
+    let notasValidas = notas.slice(1,4);
+
+    let soma = notasValidas.reduce((acc, nota) => acc + nota, 0);
+    let media = soma / notasValidas.length;
+
+    return media;
+}
+
+function processarAtletas (atletas)
+{
+    atletas.forEach(atleta => {
+        let nome = atleta.nome;
+        let notas = atleta.notas;
+        let media = calcularMedia(notas);
+        
+        console.log(`Atleta: ${nome}`);
+        console.log(`Notas Obtidas: ${notas.join(',')}`);
+        console.log(`Média Válida: ${media.toFixed(6)}`);
+        console.log('');
+    });
+}
+
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
+
+   processarAtletas (atletas);
